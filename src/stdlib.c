0daba424e0477b51c80296789f1a8357f4d5a1c5
Rei <weouthere@noweaint.com>
2023-11-23 19:06:59 -0800

    IDT initialization setup

--- src/stdlib.c
+++ src/stdlib.c
@@ -33,3 +33,65 @@ char *itoa(int value, char *str, int base)
 
     return str;
 }
+
+char *uitoa(unsigned int value, char *str, int base)
+{
+    unsigned i = 1;
+    str[0] = 0x0;
+    if(value == 0){
+        str[i] = '0';
+        i++;
+    }
+    while(value){
+        unsigned digit = value % base;
+        value /= base;
+        if(digit <= 9){
+            str[i] = digit + '0';
+        }
+        else{
+            digit -= 10;
+            str[i] = digit + 'A';
+        }
+        i++;
+    }
+    for(unsigned j = 0; j < i/2; j++){
+        char old = str[j];
+        str[j] = str[i-j-1];
+        str[i-j-1] = old;
+    }
+    return str;
+}
+
+char *lltoa(long long value, char *str, int base)
+{
+    unsigned i = 1;
+    int sign = value < 0;
+    str[0] = 0x0;
+    if(value == 0){
+        str[i] = '0';
+        i++;
+    }
+    while(value){
+        int digit = value % base;
+        if (digit < 0) digit *= -1;
+        value /= base;
+        if(digit <= 9)
+            str[i] = digit + '0';
+        else{
+            digit -= 10;
+            str[i] = digit + 'A';
+        }
+        i++;
+    }
+    if(sign && base == 10){
+        str[i] = '-';
+        i++;
+    }
+    for(int j = 0; j < i/2; j++){
+        char old = str[j];
+        str[j] = str[i-j-1];
+        str[i-j-1] = old;
+    }
+
+    return str;
+}
--- src/stdlib.h
+++ src/stdlib.h
@@ -2,6 +2,7 @@
 #define STDLIB_H
 
 char* itoa(int value, char* str, int base);
-
+char* uitoa(unsigned int value, char* str, int base);
+char* lltoa(long long value, char* str, int base);
 
 #endif
