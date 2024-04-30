8cfa4936e4d182bc70284b957b9fd4ca417dc5ff
Rei <weouthere@noweaint.com>
2024-04-29 23:40:33 -0700

    woot

--- src/paging.s
+++ src/paging.s
@@ -1,6 +1,7 @@
 
 section .text
 
+    global set_cr3
     global create_mask
     create_mask:
         mov rax, 1
@@ -12,3 +13,22 @@ section .text
             dec rdi
             jnz .cm_loop
         ret
+    set_cr3:
+        
+        mov rbx, cr0
+        and ebx, ~(1<<31)
+        mov cr0, rbx
+
+        shl rdi, 12
+        mov cr3, rdi
+
+        or ebx, (1<<31)
+        mov cr0, rbx
+
+        mov ax, 0x10
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        
+        ret 
