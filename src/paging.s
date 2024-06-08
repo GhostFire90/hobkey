0876e72ff6e9232c127a8cb341d47385713108b6
GhostFire90 <strikingfire04@gmail.com>
2024-06-07 21:34:19 -0700

    woot paging rewrite

--- src/paging.s
+++ src/paging.s
@@ -2,10 +2,11 @@
 section .text
 
     global create_mask
+    ; (1<<n)-1
     create_mask:
-        mov rax, 1
-        mov rcx, rdi
-        shl rax, cl
-        dec rax
-        ret
+        mov rax, 1      ; initialize mask
+        mov rcx, rdi    ; move rdi into rcx, since cl is the lower 8 bits of rcx
+        shl rax, cl     ; shift rax left by cl (one of the only regs allowed for rhs of this inst)
+        dec rax         ; subtract one
+        ret             ; return mask
 
