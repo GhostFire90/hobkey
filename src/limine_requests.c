beb0665252c5d2bbd005e93b28fca1f5ac72ff55
GhostFire90 <strikingfire04@gmail.com>
2024-06-15 12:42:21 -0700

    temp mapping be confusing

--- src/limine_requests.c
+++ src/limine_requests.c
@@ -20,6 +20,16 @@ static volatile struct limine_kernel_address_request kernel_addr_request = {
     .response = 0x0
 };
 
+static volatile struct limine_framebuffer_request frame_buffer_req = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+static volatile struct limine_module_request initrd_req = {
+    .id = LIMINE_MODULE_REQUEST,
+    .revision = 0
+};
+
 const struct limine_memmap_response *limine_memmap()
 {
     return memmap_req.response;
@@ -33,4 +43,14 @@ const struct limine_hhdm_response *limine_hhdm()
 const struct limine_kernel_address_response *limine_kernel_addr()
 {
     return kernel_addr_request.response;
+}
+
+const struct limine_module_response *limine_modules()
+{
+    return initrd_req.response;
+}
+
+const struct limine_framebuffer_response *limine_framebuffer()
+{
+    return frame_buffer_req.response;
 }
--- src/limine_requests.h
+++ src/limine_requests.h
@@ -6,6 +6,8 @@
 const struct limine_memmap_response* limine_memmap();
 const struct limine_hhdm_response* limine_hhdm();
 const struct limine_kernel_address_response* limine_kernel_addr();
+const struct limine_module_response* limine_modules();
+const struct limine_framebuffer_response* limine_framebuffer();
 
 
 #endif
