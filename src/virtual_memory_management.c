da0a162d3cb9598541615e3f0e3d566e37842092
GhostFire90 <strikingfire04@gmail.com>
2024-07-22 20:49:59 -0700

    Streams

--- src/virtual_memory_management.c
+++ src/virtual_memory_management.c
@@ -5,7 +5,7 @@
 #include "paging.h"
 
 
-extern void invalidate_page(uint64_t);
+extern void invalidate_page(void*);
 static uint64_t next_kernel_page;
 
 void initailize_vmm(uint64_t _next_kernel_page)
@@ -21,4 +21,32 @@ void* extend_kernel_map(void* page)
 
     next_kernel_page+=PAGE_SIZE;
     return addr;
+}
+
+void map_phy_to_vrt(void *virtual, void *physical, unsigned long flags)
+{
+    uint64_t* location = map_crawl_mark((uintptr_t) virtual, LAYER_PT, flags);
+    set_pointer(location, (uint64_t) physical, flags);
+    invalidate_page(virtual);
+    unmap_temp();
+    //set_cr3((uint64_t)pml4_location);
+}
+
+void unmap_page(void * virtual)
+{
+    free_page(virtual);
+    uint64_t* entry = map_crawl((uint64_t)virtual, LAYER_PT);
+    set_pointer(entry, 0, 0);
+    invalidate_page(entry);
+
+}
+
+void remap_page(void *old, void *new, uint64_t flags)
+{
+    uint64_t* val = map_crawl((uint64_t)old, LAYER_PT);
+    set_pointer(map_crawl_mark((uint64_t)new, LAYER_PT, flags), *val, flags);
+    set_pointer(val, 0, 0);
+    invalidate_page(old);
+    invalidate_page(new);
+    
 }
--- src/virtual_memory_management.h
+++ src/virtual_memory_management.h
@@ -3,5 +3,8 @@
 
 void initailize_vmm(unsigned long next_kernel_page);
 void* extend_kernel_map(void* page);
+void map_phy_to_vrt(void* virtual, void* physical, unsigned long  flags);
+void unmap_page(void* virtual);
+void remap_page(void* old, void* new, unsigned long flags);
 
 #endif
